ec284868bbaaf5887a26bd6c5659bc5475988e39
jkrth617 <jkrth617@gmail.com>
2015-11-16 13:23:56 -0500

    added the initial ajax call with broken link

--- js/myApp.js
+++ js/myApp.js
@@ -1,4 +1,14 @@
 $(document).on('ready', function (e) {
+
+  $.ajax({
+    url:"http://jkrth617.github.io/info/about.html",
+    type:"get",
+  }).then(function(response) {
+    $('#info-content').html(response);
+  }).fail(function(deffered) {
+    alert("Sorry, There was a problem fetching the 'about me' data");
+  });
+
   $('.navbar-nav li a').on('click', function (event) {
     event.preventDefault();
     var $self = $(this);
