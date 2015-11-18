9b547e408c19cc2b7934f087e8ac4580926d7e37
jkrth617 <jkrth617@gmail.com>
2015-11-18 13:06:10 -0500

    change to freelancer js

--- js/freelancer.js
+++ js/freelancer.js
@@ -7,11 +7,11 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('body').on('click', '.page-scroll a', function(event) {
+        event.preventDefault();
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
-        event.preventDefault();
     });
 });
 
